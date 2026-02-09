4e403712f75929f75863e8c584389ffc540a81cd
mimikhan65-cmyk <amsaljaved65@gmail.com>
2026-02-09 14:47:34 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 " 𝑭𝜟𝜧𝜣𝑼𝜹 𝜧𝜤𝜧𝜤𝜤 "                ˁܓ 𝑫𝜩𝑺𝜯𝜰𝜣𝜳𝜩𝜰ܓˀ 𓆪',
+  author: '‎',
+  botName: "𓆩" 𝑭𝜟𝜧𝜣𝑼𝜹 𝜧𝜤𝜧𝜤𝜤 "                ˁܓ 𝑫𝜩𝑺𝜯𝜰𝜣𝜳𝜩𝜰ܓˀ 𓆪",
+  botOwner: 'MIMII // Your name
+  ownerNumber: '0314 9424512', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: https://github.com/mimikhan65-cmyk/Mimi?tab=readme-ov-file
+};
+
+module.exports = settings;
